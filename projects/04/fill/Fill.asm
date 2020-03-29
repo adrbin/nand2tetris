96d5ccbc5088f778e38dc6faa3473e7aeebefab0
adrbin <35666777+adrbin@users.noreply.github.com>
2020-03-29 20:54:25 +0200

    project 4

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,45 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(MAINLOOP)
+
+@fill
+M=0
+@KBD
+D=M
+@START
+D=D;JEQ
+@fill
+M=-1
+
+(START)
+@i
+M=-1
+
+(LOOP)
+@8192
+D=A
+@i
+M=M+1 // increment i
+D=D-M // 8192 - i
+@FINISH
+D=D;JLE // if screen ended finish
+
+@i
+D=M
+@SCREEN
+D=A+D
+@screen_addr
+M=D // calculate current screen address SCREEN + i
+@fill
+D=M
+@screen_addr
+A=M
+M=D // draw 16 pixels
+@LOOP
+0;JMP // loop to draw next piexels
+
+(FINISH)
+@MAINLOOP
+0;JMP // loop to the beginning
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,33 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+@R0
+D=M
+@i
+M=D // initialize i = R0
+
+@sum
+M=0 // initialize sum = 0
+
+(LOOP)
+@i
+MD=M-1
+@FINISH
+D=D;JLT // if i < 0 end loop
+@R1
+D=M
+@sum
+M=M+D // add sum + R1
+@LOOP
+0;JMP
+
+(FINISH)
+@sum
+D=M
+@R2
+M=D // R2 = sum
+
+(END)
+@END
+0;JMP
